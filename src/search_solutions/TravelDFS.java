1424207a9abab6b2c43deea3d101f609919632e0
PandorasGit <wendallar125@gmail.com>
2024-01-23 14:03:20 -0500

    Finish travelDFS

--- src/search_solutions/TravelDFS.java
+++ src/search_solutions/TravelDFS.java
@@ -1,2 +1,18 @@
-package search_solutions;public class TravelDFS {
+package search_solutions;
+
+import core_search.BaseSearch;
+import core_search.FIFOQueue;
+import core_search.LIFOQueue;
+import search_problems.*;
+
+public class TravelDFS extends BaseSearch<String, String> {
+    public TravelDFS(String mapFile) {
+        super(new Travel(mapFile), new LIFOQueue<>());
+    }
+
+
+    public static void main(String[] args) {
+        TravelDFS t = new TravelDFS("RomaniaMap.txt");
+        t.search();
+    }
 }
