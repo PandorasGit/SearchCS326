31012b1a3e66d3e79cf20f25379f335a49e2e91a
PandorasGit <wendallar125@gmail.com>
2024-01-23 14:21:13 -0500

    Almost get UCS working

--- src/search_solutions/TravelUCS.java
+++ src/search_solutions/TravelUCS.java
@@ -1,2 +1,16 @@
-package search_solutions;public class TravelUCS {
+package search_solutions;
+
+import core_search.*;
+import search_problems.*;
+
+public class TravelUCS extends BaseSearch<String, String> {
+    public TravelUCS(String mapFile) {
+        super(new Travel(mapFile), new SortedQueue<String, String>();
+    }
+
+
+    public static void main(String[] args) {
+        search_solutions.TravelUCS t = new search_solutions.TravelUCS("RomaniaMap.txt");
+        t.search();
+    }
 }
