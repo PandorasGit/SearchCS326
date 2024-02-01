9b9514360f47731f51adbeeaee3543385f66d454
Gavin Neal <grneal@bsu.edu>
2024-02-01 15:16:20 -0500

    Add Siliding Tile AStar solution and test

---
+++ src/search_solutions/SlidingTileAStar.java
@@ -0,0 +1,38 @@
+package search_solutions;
+
+import core_search.BaseSearch;
+import core_search.Node;
+import core_search.SortedQueue;
+import search_problems.SlidingTile;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class SlidingTileAStar extends BaseSearch<ArrayList<Integer>, String> {
+    public SlidingTileAStar(ArrayList<Integer> state){
+        super(getProblem(state), getFrontier(getProblem(state)));
+    }
+
+    private static SortedQueue<ArrayList<Integer>, String> getFrontier(SlidingTile problem) {
+        return new SortedQueue<ArrayList<Integer>, String>(new Comparator<Node<ArrayList<Integer>, String>>() {
+            @Override
+            public int compare(Node<ArrayList<Integer>, String> o1, Node<ArrayList<Integer>, String> o2) {
+
+                if ((problem.misplacedTiles(o1.getState()) + o1.getPathCost()) > (problem.misplacedTiles(o2.getState()) + o2.getPathCost())) {
+                    return 1;
+                }
+                if ((problem.misplacedTiles(o1.getState()) + o1.getPathCost()) == (problem.misplacedTiles(o2.getState()) + o2.getPathCost())) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            }
+        });
+    }
+
+    private static SlidingTile getProblem(ArrayList<Integer> data) {
+        return new SlidingTile(data);
+    }
+
+}
+
